4a4d946c8a41189161164572ff573cdfbb852e73
dYnAmO25 <113728244+dYnAmO25@users.noreply.github.com>
2023-05-30 11:45:31 +0200

    worked on DayNight Cycle

--- Eco System/Assets/DayNightCycle.cs
+++ Eco System/Assets/DayNightCycle.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    private Light sun;
+    public float fSpeed;
+    public float fDayMinutes;
+    public Color dayColor;
+    public Color nightColor;
+    public Material skyboxMaterial;
+    public GameObject directionalLight;
 
-    void Start()
-    {
-        sun = GetComponent<Light>();
-    }
+    private float fRotation = 0;
+    private float fDayTime = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        fDayTime += (Time.deltaTime * fSpeed);
+
+        fRotation = fDayTime / (fDayMinutes * 60) * 360;
+
+        // Day Cycle
+        if (fRotation <= 180)
+        {
+
+        }
+        // Night Cycle
+        else
+        {
+            fRotation -= 180;
+        }
+
+        transform.rotation = Quaternion.Euler(new Vector3(fRotation, 0, 0));
+
+        if (fDayTime >= fDayMinutes * 60)
+        {
+            fDayTime = 0;
+        }
     }
 }
